b2dfeefd327d7cca6fcf49cac4a8b95deafd48ff
Guto Xavier <gutoffline@gmail.com>
2022-07-23 13:00:31 -0300

    função de exibir mensagem

--- views/formulario.py
+++ views/formulario.py
@@ -3,7 +3,13 @@ def formulario_login():
     usuario_digitado = input("Informe o seu usuário: ")
     senha_digitado = input("Informe sua senha: ")
     usuario_completo = [usuario_digitado, senha_digitado]
+    return usuario_completo
     
-
+def exibir_mensagem(texto):
+    print("\n\n")
+    print("============================")
+    print(texto)
+    print("============================")
+    print("\n\n")
 
 
